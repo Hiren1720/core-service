e589880874d8f59770c2a61bd1078769d232dcfc
Akshay <akshay10sangani@gmail.com>
2026-08-18 11:34:31 +0530

    bug fixed

--- src/controllers/performance/myAttendance.controller.ts
+++ src/controllers/performance/myAttendance.controller.ts
@@ -179,7 +179,7 @@ export const getAttendanceByMonth = async (
     return res.status(200).json(
       ApiResponse.success(
         {
-          list: [...generatedDays],
+          list: [...attendanceRecords, ...generatedDays],
         },
         "Attendance fetched successfully",
       ),
