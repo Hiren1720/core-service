d635c770f61ec42dd5123475925d07de47e64b0b
Akshay <akshay10sangani@gmail.com>
2026-09-10 18:19:11 +0530

    middlware fixed

--- src/middleware/user.middleware.ts
+++ src/middleware/user.middleware.ts
@@ -1,69 +1,53 @@
-import {
-    NextFunction,
-    Request,
-    Response,
-} from "express";
+import { NextFunction, Request, Response } from "express";
 
-import {
-    verifyAccessToken,
-} from "../shared/utils/jwt.js";
+import { verifyAccessToken } from "../shared/utils/jwt.js";
 import { UserModel } from "../infrastructure/database/models/index.js";
 import { userStatus } from "../types/types.js";
 
 export const authenticateUser = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
-    try {
-        const authHeader =
-            req.headers.authorization;
+  try {
+    const authHeader = req.headers.authorization;
 
-        if (
-            !authHeader ||
-            !authHeader.startsWith(
-                "Bearer "
-            )
-        ) {
-            return res.status(401).json({
-                success: false,
-                message:
-                    "Authentication required",
-            });
-        }
-
-        const token =
-            authHeader.split(" ")[1];
-
-        const decoded =
-            verifyAccessToken(token);
-
-        const user = await UserModel.findOne({
-            _id: decoded.userId,
-            status: userStatus.ACTIVE,
-        }).lean();
-
-        if (!user) {
-            res.status(401).json({
-                success: false,
-                message: "User account is not active",
-            });
-            return;
-        }
-
-        req.user = {
-            id: decoded.userId,
-            companyId:
-                decoded.companyId,
-            role: decoded.role,
-        };
-
-        next();
-    } catch {
-        return res.status(401).json({
-            success: false,
-            message:
-                "Invalid or expired token",
-        });
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication required",
+      });
     }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = verifyAccessToken(token);
+
+    if (decoded.role === "ADMIN") {
+      const user = await UserModel.findOne({
+        _id: decoded.userId,
+        status: userStatus.ACTIVE,
+      }).lean();
+      if (!user) {
+        res.status(401).json({
+          success: false,
+          message: "User account is not active",
+        });
+        return;
+      }
+    }
+
+    req.user = {
+      id: decoded.userId,
+      companyId: decoded.companyId,
+      role: decoded.role,
+    };
+
+    next();
+  } catch {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
 };
